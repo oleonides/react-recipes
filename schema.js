34418e8631904e39b7780b2b1dd11d854cb05579
oleonides <olvert.leonides@gmail.com>
2019-02-10 18:22:30 -0700

    first commit

---
+++ schema.js
@@ -0,0 +1,28 @@
+exports.typeDefs = `
+
+type Recipe {
+    _id: ID
+    name: String!
+    imageUrl: String!
+    category: String!
+    description: String!
+    instructions: String!
+    createdDate: String
+    likes: Int
+    username: String
+  }
+
+type User {
+    _id: ID
+    username: String! @unique
+    password: String!
+    email: String!
+    joinDate: String
+    favorites: [Recipe]
+  }
+
+  type Query {
+    getAllRecipes: [Recipe]
+  }
+
+  `;
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+const bodyParser = require("body-parser");
+
+require("dotenv").config({ path: "variables.env" });
+
+const Recipe = require("./models/Recipe");
+const User = require("./models/User");
+
+//Bring in GraphQL-Express middleware
+const { graphiqlExpress, graphqlExpress } = require("apollo-server-express");
+const { makeExecutableSchema } = require("graphql-tools");
+
+const { typeDefs } = require("./schema");
+const { resolvers } = require("./resolvers");
+
+//create schema
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+});
+
+//connect to DB
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.log(err));
+
+//Initialize application
+const app = express();
+
+//Create GraphQL application
+app.use("/graphiql", graphiqlExpress({ endpointURL: "/graphql" }));
+
+//Connect schemas with GraphQL
+app.use(
+  "/graphql",
+  bodyParser.json(),
+  graphqlExpress({
+    schema,
+    context: {
+      Recipe,
+      User
+    }
+  })
+);
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});

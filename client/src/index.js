c46b34dae9282d1a70b72abcbda0a7d82017a116
Olvert Leonides <olvert.leonides@gmail.com>
2021-01-04 21:39:45 -0700

    Feat: create client app

--- client/src/index.js
+++ client/src/index.js
@@ -7,7 +7,7 @@ import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
 
 const client = new ApolloClient({
-  uri: "http://localhost:4444/graphql"
+  uri: "http://localhost:5000/graphql",
 });
 
 ReactDOM.render(
@@ -17,6 +17,6 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
